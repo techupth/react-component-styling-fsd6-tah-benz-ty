78b6e043cddaf769add7533b92191acc340cdf4a
Your Name <you@example.com>
2024-06-04 20:32:56 +0700

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
+import { Button } from "./components/Button";
+import { Alert } from "./components/Alert";
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
         {/* Render ตัว Button 2 แบบ */}
+        <Button color="primary" text="Primary" />
+        <Button color="secondary" text="Secondary" />
       </div>
       <hr />
       <div className="alert-components-section">
         {/* Render ตัว Alert 4 แบบ */}
+        <Alert severity="error" message="This is error alert box" />
+        <Alert severity="warning" message="This is warning alert box" />
+        <Alert severity="info" message="This is info alert box" />
+        <Alert severity="success" message="This is success alert box" />
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,30 @@
 // Start coding here
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  let bgColor = undefined;
+  if (props.color === "primary") {
+    bgColor = "#074ee8";
+  } else if (props.color === "secondary") {
+    bgColor = "#07A4E8";
+  }
+}
+return (
+  <button
+    css={css`
+       display: block;
+       background-color: ${bgColor};
+       margin: 10px;
+       width: 170px;
+       height: 50px;
+       border: none;
+       border-radius: 40px;
+       color: whight: 400;
+       font-size: 16px;
+      `}
+  >
+    {props.text}
+  </button>
+);
+
+export default Button;
