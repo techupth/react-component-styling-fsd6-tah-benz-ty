5e9b97f16fc1cfbf528f8078819f20303ff36a49
Your Name <you@example.com>
2024-06-04 20:42:30 +0700

    feat

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,55 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+// ในตัวอย่างเฉลยเราจะใช้ SVG แต่ถ้าใครใช้รูปภาพก็สามารถใช้แนวทางนี้ได้เหมือนกัน
+import ErrorIcon from "../assets/icons/frown.jsx";
+import WarningIcon from "../assets/icons/alert-triangle.jsx";
+import InfoIcon from "../assets/icons/alert-circle.jsx";
+import SuccessIcon from "../assets/icons/check-circle.jsx";
+
+function Alert(props) {
+  let bgColor = "#F9EBC8";
+  let AlertIcon = InfoIcon;
+
+  if (props.severity === "error") {
+    bgColor = "#F9C8C8";
+    AlertIcon = ErrorIcon;
+  } else if (props.severity === "warning") {
+    bgColor = "#F9D9C8";
+    AlertIcon = WarningIcon;
+  } else if (props.severity === "success") {
+    bgColor = "#CEF7CD";
+    AlertIcon = SuccessIcon;
+  }
+
+  return (
+    <div
+      css={css`
+        margin: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        padding-left: 20px;
+        width: 650px;
+        height: 76px;
+        font-family: "Kanit", sans-serif;
+        font-weight: 700;
+        font-size: 20px;
+        color: #444444;
+        border-radius: 10px;
+        background-color: ${bgColor};
+      `}
+    >
+      <div
+        css={css`
+          margin-right: 20px;
+        `}
+      >
+        <AlertIcon />
+      </div>
+      {props.message}
+    </div>
+  );
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
 function Button(props) {
@@ -6,25 +7,27 @@ function Button(props) {
   if (props.color === "primary") {
     bgColor = "#074ee8";
   } else if (props.color === "secondary") {
-    bgColor = "#07A4E8";
+    bgColor = "#07a4e8";
   }
-}
-return (
-  <button
-    css={css`
-       display: block;
-       background-color: ${bgColor};
-       margin: 10px;
-       width: 170px;
-       height: 50px;
-       border: none;
-       border-radius: 40px;
-       color: whight: 400;
-       font-size: 16px;
+  return (
+    <button
+      css={css`
+        display: block;
+        background-color: ${bgColor};
+        margin: 10px;
+        width: 170px;
+        height: 50px;
+        border: none;
+        border-radius: 4px;
+        color: white;
+        font-weight: 400;
+        font-size: 16px;
+        font-family: "Sarabun", sans-serif;
       `}
-  >
-    {props.text}
-  </button>
-);
+    >
+      {props.text}
+    </button>
+  );
+}
 
 export default Button;
